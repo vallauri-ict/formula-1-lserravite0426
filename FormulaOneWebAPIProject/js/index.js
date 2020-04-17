7d408fd3aa802c806477963236473ab6407f8e67
lserravite0426 <57641140+lserravite0426@users.noreply.github.com>
2020-04-17 22:15:51 +0200

    Implementazione ricerca client WebAPI.

--- FormulaOneWebAPIProject/js/index.js
+++ FormulaOneWebAPIProject/js/index.js
@@ -1,35 +1,55 @@
 ﻿"use strict";
 
+let app;
+
 $(function () {
-    let app = new Vue({
+    app = new Vue({
         el: "#app",
         data: {
             data: [],
             rows: [],
             dataRequired: '',
-            disableButtons: false
+            disableButtons: false,
+            searchID: '',
+            notFound: false
         },
         methods: {
             request: function (uri) {
                 this.disableButtons = true;
                 this.data = [];
                 this.rows = [];
+                this.searchID = '';
+                this.notFound = false;
                 this.dataRequired = uri;
                 $.getJSON('/api/' + uri)
                     .done(data => {
                         console.log(data);
                         this.data = data;
-                        for (let i = 0; i < this.data.length; i += 3) {
-                            this.rows[i] = this.data.slice(i, i + 3);
-                        }
+                        createRow();
                     })
                     .fail((jqxhr, textStatus, error) => {
                         console.log(jqxhr, textStatus, error);
                     })
                     .always(() => this.disableButtons = false);
+            },
+            search: function () {
+                this.notFound = false;
+                if (this.searchID == '')
+                    createRow();
+                else {
+                    let item = this.data.find(i => i.id == this.searchID);
+                    if (item == undefined)
+                        this.notFound = true;
+                    else
+                        this.rows = [[item]];
+                }
             }
         }
     });
-
-    
 });
+
+function createRow() {
+    for (let i = 0; i < app.data.length; i += 3) {
+        app.rows[i] = app.data.slice(i, i + 3);
+    }
+}
